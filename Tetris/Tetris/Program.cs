55aa3072b35dfaca79392ef5d2020777a0588c76
nuntiusnix <accipitplagameden@gmail.com>
2021-09-28 22:16:12 +0300

    Мы создали класс Point.

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,7 +6,27 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            Point p1 = new Point();
+
+            p1.x = 2;
+            p1.y = 3;
+            p1.c = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+
+            p2.x = 5;
+            p2.y = 4;
+            p2.c = '*';
+            p2.Draw();
+
+            Console.ReadLine();
+
         }
+
+        
     }
 }
